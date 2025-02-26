fe70f0c52435b8737a2379f5f1670cad566e738b
BoBenc <bogerbence2002@gmail.com>
2025-02-26 14:58:47 +0100

    UserController kezdés

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function register() {}
+
+    public function login() {}
+
+    public function logout() {}
 }
