36f9f4df51f7627b71ac4da140cdc8d103fb9901
BoBenc <bogerbence2002@gmail.com>
2025-02-26 15:58:47 +0100

    User és Customers request-ek beállítása

---
+++ app/Http/Requests/UserRegisterRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UserRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name" => "required|max:15|unique:users,name",
+            "email" => "required|email|unique:users,email",
+            "password" => ["required","min:6","regex:/[a-z]/","regex:/[A-Z]/","regex:/[0-9]/" ],
+            "confirm_password" => "required|same:password",
+            "phone" => "required|unique:users,phone",
+            "address" => "required",
+            "birth_date" => "required"
+        ];
+    }
+
+    public function messages() {
+        return [
+            "name.required" => "Név kötelező!",
+            "name.max" => "Túl hosszú név!",
+            "name.unique" => "Létező név!",
+            "email.required" => "E-mail kötelező!",
+            "email.unique" => "Létező e-mail!",
+            "password.required" => "Jelszó kötelező!",
+            "password.min" => "Túl rövid jelszó!",
+            "password.regex" => "A jelszónak tartalmazia kell kisbetűt, nagybetűt és számot!",
+            "confirm_password.same" => "A két jelszó nem egyezik!",
+            "phone.required" => "Telefonszám kötelező!",
+            "phone.unique" => "Telefonszám létezik!",
+            "address.required" => "Cím megadása kötelező!",
+            "birth_date.required" => "Születési dátum megadása kötelező!"
+        ];
+    }
+
+    public function failedValidation(Validator $validator) {
+        throw new HttpResponseException(response()->json([
+            "success" => false,
+            "message" => "Beviteli hiba",
+            "data" => $validator -> errors()
+        ]));
+    }
+}
