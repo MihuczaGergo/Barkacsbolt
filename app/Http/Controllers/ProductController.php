d4a9da05c2f886633ceff5b3aad4fc9232bf208c
Mihucza Gergo <mihuczagergo@ktch.hu>
2025-02-26 14:53:46 +0100

    productcontroller kezdése

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function getProduct(){
+        $products = Product::all();
+        return view('product', ['products' => $products]);
+
+    }
+
+    public function addProduct(Request $request)
+    {
+        $product = new Product();
+        $product->name = $request->input('name');
+        $product->price = $request->input('price');
+        $product->description = $request->input('description');
+        $product->save();
+
+        return response()->json(['success' => 'Termék sikeresen felvéve!']);
+    }
 }
