1562a7580334f88fd5a2255e17b3f7b21c9e3fe9
BoBenc <bogerbence2002@gmail.com>
2025-02-26 15:22:43 +0100

    (User) -> Model beállítása és Controller szekesztése

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function register() {}
+    public function addUser(Request $request) {
+        $user = User::create([
+            "name" => $request["name"],
+            "password" => bcrypt($request["password"]),
+            "phone" => $request["phone"],
+            "email" => $request["email"],
+            "address" => $request["address"],
+            "birth_date" => $request["birth_date"]
+        ]);
+        return response() -> json([$name, "Sikeres dolgozó felvétel!"]);
+    }
 
-    public function login() {}
+    public function login(Request $request) {
+        if(Auth::attempt(["email" => $request["email"], "password" => $request["password"]])) {
+            $authUser = Auth::user();
+                $token = $authUser->createToken($authUser->name."Token")->plainTextToken;
+                $data["user"] = ["user" => $authUser->name];
+                $data["token"] = $token;
+                return response() -> json([$data, "Sikeres bejelentkezés!"]);
+            }
+        else {
+            return response() -> json("Hibás e-mail vagy jelszó!");
+        }
+    }
 
-    public function logout() {}
+    public function logout(Request $request) {
+        auth("sanctum") -> user() -> currentAccessToken() -> delete();
+        $name = auth("sanctum") -> user() -> name;
+        return response() -> json([$name, "Sikeresen kijelentkezett!"]);
+    }
 }
