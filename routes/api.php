7eb313cef60c22ac0765ae266f43f245fa0c845a
BoBenc <bogerbence2002@gmail.com>
2025-03-05 14:37:47 +0100

    Customers törlése

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post("/register", [UserController::class, "register"]);
+Route::post("/login", [UserController::class, "login"]);
+Route::post("/logout", [UserController::class, "logout"]);
 
 Route::post('/addProduct',[ProductController::class, 'addProduct']);
 Route::post('/addcategory',[CategoryController::class, 'addCategory']);
